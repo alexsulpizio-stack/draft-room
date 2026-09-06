53d489adf5f1d182a3faf933a39bcebe8d2a8a08
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-06 01:54:53 -0400

    Add isolated Yahoo relay topic

--- src/lib/relay-urls.ts
+++ src/lib/relay-urls.ts
@@ -2,5 +2,7 @@
 export const NTFY_HOST = "https://ntfy.sh";
 export const ESPN_RELAY_TOPIC = "drjfl28jackal";
 export const RANKS_RELAY_TOPIC = "drjfl28jackal-ranks";
+export const YAHOO_RELAY_TOPIC = "drjfl28jackal-yahoo";
 export const ESPN_RELAY_URL = `${NTFY_HOST}/${ESPN_RELAY_TOPIC}`;
 export const RANKS_RELAY_URL = `${NTFY_HOST}/${RANKS_RELAY_TOPIC}`;
+export const YAHOO_RELAY_URL = `${NTFY_HOST}/${YAHOO_RELAY_TOPIC}`;
